5e3e58cb5fa43c75d5e40a2643c643b6a93c3e72
aubin8764 <aubin.souvray19@gmail.com>
2025-12-29 15:09:51 +0100

    Adding animation curve for jump

--- Assets/Component/Player/Scripts/PlayerMovementController.cs
+++ Assets/Component/Player/Scripts/PlayerMovementController.cs
@@ -11,6 +11,8 @@ public class PlayerMovementController : MonoBehaviour
     [Header("Jump Parameters")]
     [SerializeField] private float _jumpDuration = 1f;
     [SerializeField] private float _jumpHeight = 2f;
+    [SerializeField] private AnimationCurve _jumpCurve;
+    [SerializeField] private AnimationCurve _fallCurve;
 
     private void Update()
     {
@@ -36,7 +38,10 @@ public class PlayerMovementController : MonoBehaviour
             jumpTimer += Time.deltaTime;
             float normalizedTime = jumpTimer / halfJumpDuration;
 
-            float targetHeight = Mathf.Lerp(0, _jumpHeight, normalizedTime);
+            //float targetHeight = Mathf.Lerp(0, _jumpHeight, normalizedTime);
+
+            var targetHeight = _jumpCurve.Evaluate(normalizedTime) * _jumpHeight;
+
             var taegetPosition = new Vector3(transform.position.x, targetHeight, transform.position.z);
             transform.position = taegetPosition;
 
@@ -52,7 +57,10 @@ public class PlayerMovementController : MonoBehaviour
             jumpTimer += Time.deltaTime;
             var normalizedTime = Mathf.Clamp01(jumpTimer / halfJumpDuration);
 
-            float targetHeight = Mathf.Lerp(_jumpHeight, 0, normalizedTime);
+            //float targetHeight = Mathf.Lerp(_jumpHeight, 0, normalizedTime);
+
+            var targetHeight = _fallCurve.Evaluate(normalizedTime) * _jumpHeight; 
+
             var targetPosition = new Vector3(transform.position.x, targetHeight, transform.position.z);
             transform.position = targetPosition;
 
