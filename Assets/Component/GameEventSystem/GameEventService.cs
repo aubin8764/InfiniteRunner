404c31a7538e8da9e8fa1941cb1ff0405d7da488
aubin8764 <aubin.souvray19@gmail.com>
2026-01-28 17:02:36 +0100

    Add scoring count

--- Assets/Component/GameEventSystem/GameEventService.cs
+++ Assets/Component/GameEventSystem/GameEventService.cs
@@ -5,6 +5,8 @@ public static class GameEventService
     public static Action OnCollision;
     public static Action<int> OnPlayerLifeUpdated;
     public static Action<float> OnCountdownTick;
+
+    public static Action<int> OnScoreUpdated;
     
     public static Action<bool> OnCountdownState;
     public static Action<bool> OnGameState;
